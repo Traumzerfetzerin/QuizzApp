4acbb968cf8da3847aed635234ba85b522a0f40c
Traumzerfetzerin <nadjaparadiesvogel@gmail.com>
2024-04-29 21:05:13 +0200

    added function

--- script.js
+++ script.js
@@ -1,50 +1,76 @@
+let currentQuestion = 0;
 let questions = [
     {
-        'question': 'Wer hat HTML erfunden?',
-        'answer_1': 'Robbie Williams',
-        'answer_2': 'Lady Gaga',
-        'answer_3': 'Tim Berners-Lee',
-        'answer_4': 'Justin Bieber',
-        'right_answer': 3,
-    },
-    {
-        'question': 'Was bedeutet das HTML Tag &lt;a&gt;?',
-        'answer_1': 'Text Fett',
-        'answer_2': 'Container',
-        'answer_3': 'Ein Link',
-        'answer_4': 'Kursiv',
-        'right_answer': 3,
-    },
-    {
-        'question': 'Wie bindet man eine Webseite in eine Webseite ein?',
-        'answer_1': '&lt;iframe&gt;, &lt;frame&gt; and &lt;frameset&gt',
-        'answer_2': '&lt;iframe&gt;',
-        'answer_3': '&lt;frame&gt;',
-        'answer_4': '&lt;frameset&gt;',
+        'question': 'Was ist ein Byte?',
+        'answer_1': 'Teil der Hardware',
+        'answer_2': 'Eine Maßeinheit für Datenmengen',
+        'answer_3': 'Programmiersprache',
+        'answer_4': 'Ein Bildbearbeitungsprogramm',
         'right_answer': 2,
     },
     {
-        'question': 'Welches Attribut kann man NICHT für Textarea verwenden?',
-        'answer_1': 'readonly',
-        'answer_2': 'max',
-        'answer_3': 'from',
-        'answer_4': 'spellcheck',
-        'right_answer': 1,
-    },
-    {
-        'question': 'Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem Attribut title aus?',
-        'answer_1': 'a[title]{...}',
-        'answer_2': 'a > title {...}',
-        'answer_3': 'a.title {...}',
-        'answer_4': 'a=title {...}',
-        'right_answer': 1,
-    },
-    {
-        'question': 'Wie definiert man in JavaScript eine Variable?',
-        'answer_1': 'let 100 = rate;',
-        'answer_2': '100 = let rate;',
-        'answer_3': 'rate = 100;',
-        'answer_4': 'let rate = 100;',
+        'question': 'Vervollständige die Zahlenfolge: 2-7-6-11-10-15-14-?',
+        'answer_1': '16',
+        'answer_2': '17',
+        'answer_3': '18',
+        'answer_4': '19',
         'right_answer': 4,
     },
+    {
+        'question': 'Was bedeutet die Abkürzung IT?',
+        'answer_1': 'Intelligenztest',
+        'answer_2': 'Informationstechnologie',
+        'answer_3': 'Informatik und Technik',
+        'answer_4': 'Integrationstaktik',
+        'right_answer': 3,
+    },
+    {
+        'question': 'Welches Ergebnis ist korrekt?',
+        'answer_1': '502',
+        'answer_2': '220',
+        'answer_3': '121',
+        'answer_4': '402',
+        'right_answer': 1,
+    },
+    {
+        'question': 'Was ist eine MAC-Adresse?',
+        'answer_1': ' Eine spezielle E-Mail-Adresse auf einem Apple-Macintosh-Rechner',
+        'answer_2': 'Eine Reihenfolge aus Zahlen und Buchstaben, die einen Computer eindeutig identifiziert',
+        'answer_3': 'Steht für Multiple Access Cache und erlaubt den Zugriff auf verschlüsselte Seiten',
+        'answer_4': 'Die Kontaktadresse des Geräteherstellers',
+        'right_answer': 2,
+    },
+    {
+        'question': 'Wie lautet der Binärcode für die Dezimalzahl 20?',
+        'answer_1': '10101',
+        'answer_2': '10011',
+        'answer_3': '10110',
+        'answer_4': '10100',
+        'right_answer': 4,
+    },
+    {
+        'question': 'Till ist älter als Karla, aber jünger als Ole. Ella ist älter als Ole. Ordne die Namen der vier Kinder nach deren Alter. Beginne dabei mit dem Namen des ältesten Kindes.',
+        'answer_1': 'Ole, Ella, Till, Karla',
+        'answer_2': 'Till, Ella, Ole, Karla',
+        'answer_3': 'Ella, Ole, Till, Karla',
+        'answer_4': 'Ella, Till, Ole, Karla',
+        'right_answer': 3,
+    },
 ];
+
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
